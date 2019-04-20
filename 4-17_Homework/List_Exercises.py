fefe7c65a93d0210c0455b783b69dd1623512b38
GGCasual <gc.favorito95@gmail.com>
2019-04-20 00:37:02 -0500

    Week 1

--- 4-17_Homework/List_Exercises.py
+++ 4-17_Homework/List_Exercises.py
@@ -1,31 +1,33 @@
-1. Sum the Numbers
+# 1. Sum the Numbers
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(sum(nums))
+
+# 2. Largest Number
 
 
-2. Largest Number
+# 3. Smallest Number
 
 
-3. Smallest Number
+# 4. Even Numbers
 
 
-4. Even Numbers
+# 5. Positive Numbers
 
 
-5. Positive Numbers
+# 6. Positive Numbers II
 
 
-6. Positive Numbers II
+# 7. Multiply a list
 
 
-7. Multiply a list
-
-
-8. Multiply Vectors
+# 8. Multiply Vectors
 
 
 
-9. Matrix Addition
+# 9. Matrix Addition
 
 
-10. Matrix Addition II
+# 10. Matrix Addition II
 
-11. De-dup
+# 11. De-dup
