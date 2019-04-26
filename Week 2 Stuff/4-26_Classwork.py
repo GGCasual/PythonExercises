1f65be8d72f2e359064d062948bf6e98e70fdf36
GGCasual <gc.favorito95@gmail.com>
2019-04-26 18:58:54 -0500

    week 2

--- Week 2 Stuff/4-26_Classwork.py
+++ Week 2 Stuff/4-26_Classwork.py
@@ -1,26 +1,48 @@
-class Character(object):
-    def __init__(self, health, power):
-        self.health = health
-        self.power = power
-    def printMyself(self):
-        print("I am a hero.")
-    def attack(self):
-        pass
+# class Character(object):
+#     def __init__(self, health, power):
+#         self.health = health
+#         self.power = power
+#     def printMyself(self):
+#         print("I am a hero.")
+#     def attack(self):
+#         pass
 
-class Hero(Character):
-    def __init__(self, health, power):
-        super(Hero, self).__init__(health, power) ## super(works here too)
-
-
-class Goblin(Character):
-    pass
+# class Hero(Character):
+#     def __init__(self, health, power):
+#         super(Hero, self).__init__(health, power) ## super(works here too)
 
 
+# class Goblin(Character):
+#     pass
 
-hero = Hero(5, 10)
-goblin = Goblin(4, 8)
 
 
-print(hero.health)
-hero.printMyself()
-goblin.printMyself()
+# hero = Hero(5, 10)
+# goblin = Goblin(4, 8)
+
+
+# print(hero.health)
+# hero.printMyself()
+# goblin.printMyself()
+
+
+
+
+
+
+
+# file_handler = open('file.txt', 'r')
+
+# contents = file_handler.read()
+
+# file_handler.close()
+
+# print(contents)
+
+file_handler = open('file.txt', 'w')
+file_handler.write("Thank God it\'s Friday")
+file_handler.close()
+
+
+## For Friday's Homework
+python3 -m pip install --user numpy
